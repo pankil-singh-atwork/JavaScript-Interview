0cb9ca1727febf348385326c4e92e6c6e9d404bc
pankil-singh-atwork <pankilkumar.singh2020@vitstudent.ac.in>
2025-01-02 00:09:56 +0530

    hoisting in JS

---
+++ Hoisting.js
@@ -0,0 +1,15 @@
+// defining the function
+function getName() {
+    console.log("Namastey JavaScript");
+}
+getName();
+
+console.log(getName);
+// defining the variable & accessing before the initialization
+console.log(x);
+var x = 7;
+// function expression & accessing before the initialization
+myFunc();
+var myFunc = function() {
+    console.log("This is my Function");
+}
