2430cb4f6f88b6de62b04bdfc5e3d74b987b4de5
pankil-singh-atwork <pankilkumar.singh2020@vitstudent.ac.in>
2025-01-08 22:00:14 +0530

    stock ticjer project assignment completed

--- StockTickerApplication/fakeStockAPI.js
+++ StockTickerApplication/fakeStockAPI.js
@@ -1,9 +1,16 @@
 export default function getStockData() {
-    return {
-        name: 'QtechAI',
-        sym: 'QTA',
-        price: Math.random(),/* return a random number between 0 and 3 to two decimal places */
-        time: new Date()/* return a timestamp in this format: hh/mm/ss */
-    }
+  return {
+    name: "QtechAI",
+    sym: "QTA",
+    price: (Math.random() * 3).toFixed(
+      2
+    ) /* return a random number between 0 and 3 to two decimal places */,
+    time: function() {
+        const now = new Date();
+        const hour = String(now.getHours()).padStart(2 , '0')
+        const minutes = String(now.getMinutes()).padStart(2 , '0')
+        const seconds = String(now.getSeconds()).padStart(2 , '0')
+        return `${hour}/${minutes}/${seconds}`
+    }   /* return a timestamp in this format: hh/mm/ss */,
+  };
 }
-  
--- StockTickerApplication/index.js
+++ StockTickerApplication/index.js
@@ -1,16 +1,28 @@
-function renderStockTicker(stockData) {
-    const stockDisplayName = document.getElementById('name')
-    const stockDisplaySymbol = document.getElementById('symbol')
-    const stockDisplayPrice = document.getElementById('price')
-    const stockDisplayPriceIcon = document.getElementById('price-icon')
-    const stockDisplayTime = document.getElementById('time')
-    stockDisplayName.innerText = stockData.name;
-    stockDisplaySymbol.innerText = stockData.sym;
-    stockDisplayPrice.innerHTML = stockData.price;
-    stockDisplayTime.innerHTML = stockData.time;
-}
-
+import getStockData from "./fakeStockAPI.js";
 setInterval(() => {
-  const freshData = getstockData();
+  const freshData = getStockData();
   renderStockTicker(freshData);
-},1500)
+}, 1500);
+
+let prevPrice = null;
+
+function renderStockTicker(stockData) {
+  const stockDisplayName = document.getElementById("name");
+  const stockDisplaySymbol = document.getElementById("symbol");
+  const stockDisplayPrice = document.getElementById("price");
+  const stockDisplayPriceIcon = document.getElementById("price-icon");
+  const stockDisplayTime = document.getElementById("time");
+  const {name , sym , price , time} = stockData;
+  stockDisplayName.innerText = name;
+  stockDisplaySymbol.innerText = sym;
+  stockDisplayPrice.innerHTML = price;
+  stockDisplayTime.innerHTML = time();
+  // using the ternary operator 
+  const priceDirectionIcon = price > prevPrice ? 'green.svg' : price < prevPrice ? 'red.svg' : 'grey.svg'
+  const priceIconElement = document.createElement('img');
+  priceIconElement.src = `svg/${priceDirectionIcon}`
+  priceIconElement.alt = 'Stock Direction Icon'
+  stockDisplayPriceIcon.innerHTML = ''; 
+  stockDisplayPriceIcon.appendChild(priceIconElement);
+  prevPrice = price;
+}
